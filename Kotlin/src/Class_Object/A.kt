2a65d3cf96cff62d13e99034fc8a89065a0fff24
Chan <ieejo716@naver.com>
2021-06-16 15:03:21 +0900

    Kotlin basic

---
+++ Kotlin/src/Class_Object/A.kt
@@ -0,0 +1,9 @@
+package Class_Object
+
+class A(var name: String, var old: Int) {}
+
+fun main() {
+    val name = A("name", 3)
+    println("name : ${name.name}")
+    println("old : ${name.old}")
+}
